f410ace55072b1bf0d6e58ace742beb627768607
Kobi Hanoch <kobikobi622@gmail.com>
2026-08-05 21:28:11 +0300

    Added DB migrations infrastructure with docker compose set up

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
+
+const nodeEnv = process.env.NODE_ENV ?? 'development';
+dotenv.config({ path: `.env.${nodeEnv}` });
+
+export default defineConfig({
+  dialect: 'postgresql',
+  schema: './src/infrastructure/db/schema/*.schema.ts',
+  out: './migrations',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
